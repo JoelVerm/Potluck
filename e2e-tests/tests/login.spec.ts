e216e84ad45d63dde79cf159f951ad5a072cc5df
Joël Vermeulen <joel.vermeulen5@outlook.com>
2024-12-13 15:51:52 +0100

    Increase test timeout

--- e2e-tests/tests/login.spec.ts
+++ e2e-tests/tests/login.spec.ts
@@ -4,10 +4,10 @@ import * as path from "path";
 const authFile = path.join(__dirname, '../playwright/.cache/login.json');
 
 test('Be able to register', async ({page}) => {
-    const FIVE_MINUTES_MS = 5 * 60 * 1000;
-    await page.goto('http://localhost/', {timeout: FIVE_MINUTES_MS});
+    const TIMEOUT = 10 * 60 * 1000;
+    await page.goto('http://localhost/', {timeout: TIMEOUT});
     if (await page.getByTestId('tab-Home').isVisible()) return;
-    await page.getByTestId('register-tab').click({timeout: FIVE_MINUTES_MS});
+    await page.getByTestId('register-tab').click({timeout: TIMEOUT});
     await page.getByTestId('register-email').fill('a@b.c');
     await page.getByTestId('register-password').fill('AbcAbcAbcAbcAbc');
     await page.getByTestId('register-password-2').fill('AbcAbcAbcAbcAbc');
@@ -15,6 +15,6 @@ test('Be able to register', async ({page}) => {
     await page.getByTestId('tab-Settings').click();
     await page.getByTestId('new-house-name').fill('TestHouse');
     await page.getByTestId('new-house').click();
-    await expect(page.getByTestId('edit-house-name')).toBeVisible({timeout: FIVE_MINUTES_MS});
+    await expect(page.getByTestId('edit-house-name')).toBeVisible({timeout: TIMEOUT});
     await page.context().storageState({path: authFile});
 });
