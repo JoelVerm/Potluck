541fc0c0f189df1d87375d8c112dcec07e922f60
Joel Vermeulen <joel.vermeulen5@outlook.com>
2025-01-16 17:05:26 +0100

    Improve CI/CD

---
+++ e2e-tests/tests/login.spec.ts
@@ -0,0 +1,21 @@
+import {expect, test} from '@playwright/test';
+import * as path from "path";
+
+const authFile = path.join(__dirname, '../playwright/.cache/login.json');
+
+test('Be able to register', async ({page}) => {
+    const TIMEOUT = 5 * 60 * 1000;
+    test.setTimeout(TIMEOUT);
+    await page.goto('http://localhost/', {timeout: TIMEOUT});
+    if (await page.getByTestId('tab-Home').isVisible()) return;
+    await page.getByTestId('register-tab').click({timeout: TIMEOUT});
+    await page.getByTestId('register-email').fill('a@b.c');
+    await page.getByTestId('register-password').fill('AbcAbcAbcAbcAbc');
+    await page.getByTestId('register-password-2').fill('AbcAbcAbcAbcAbc');
+    await page.getByTestId('register-button').click();
+    await page.getByTestId('tab-Settings').click();
+    await page.getByTestId('new-house-name').fill('TestHouse');
+    await page.getByTestId('new-house').click();
+    await expect(page.getByTestId('edit-house-name')).toBeVisible({timeout: TIMEOUT});
+    await page.context().storageState({path: authFile});
+});
